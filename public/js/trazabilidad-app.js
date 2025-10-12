2c9a1c1be71619d423395002f010a744c4f63dd5
JOSE ANDRES GARCIA BURGOS <andresgarcia.kaox@gmail.com>
2025-10-12 08:23:44 -0500

    -

--- public/js/trazabilidad-app.js
+++ public/js/trazabilidad-app.js
@@ -135,15 +135,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         </div>
                     </div>
                     <div class="flex flex-wrap gap-2 items-center justify-start sm:justify-end flex-shrink-0">
+                        <button class="text-xs bg-sky-600 hover:bg-sky-700 text-white p-2 rounded-lg" data-id="${batchData.id}" title="Ver Trazabilidad">
+                            <a href="/${batchData.id}" target="_blank"><i class="fa-solid fa-eye"></i></a>
+                        </button>
                         <button class="qr-btn text-xs bg-sky-600 hover:bg-sky-700 text-white p-2 rounded-lg" data-id="${batchData.id}" title="Generar QR">
-                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 3a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1H4a1 1 0 01-1-1V3zm2 2a1 1 0 100-2 1 1 0 000 2zM3 11a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1H4a1 1 0 01-1-1v-4zm2 2a1 1 0 100-2 1 1 0 000 2zM11 3a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1V3zm2 2a1 1 0 100-2 1 1 0 000 2zM12 11a1 1 0 00-1 1v1h1a1 1 0 110 2h-1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1v-1a1 1 0 00-1-1zm-1 5a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm2 0a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" /></svg>
+                            <i class="fa-solid fa-qrcode"></i>
                         </button>
                         <button class="edit-btn text-xs bg-stone-200 hover:bg-stone-300 p-2 rounded-lg" data-batch-id="${batchData.id}" data-template-id="${template.id}" data-stage-id="${stage.id}" title="Editar">
-                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" /><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" /></svg>
+                            <i class="fa-solid fa-pen-to-square"></i>
                         </button>
                         <button class="delete-btn text-xs bg-red-600 hover:bg-red-700 text-white p-2 rounded-lg" data-batch-id="${batchData.id}" title="Eliminar">
-                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clip-rule="evenodd" /></svg>
-                        </button>
+                            <i class="fa-solid fa-trash"></i>                        </button>
                         ${nextStage ? `<button ${outputWeight <= 0 ? 'disabled' : ''} class="add-sub-btn text-xs bg-green-600 hover:bg-green-700 text-white font-bold px-3 py-1.5 rounded-lg disabled:bg-gray-400" data-parent-id="${batchData.id}" data-template-id="${template.id}" data-next-stage-id="${nextStage.id}">+ Añadir ${nextStage.nombre_etapa}</button>` : ''}
                     </div>
                 </div>
@@ -456,6 +458,18 @@ document.addEventListener('DOMContentLoaded', () => {
             link.href = qr.createDataURL(4, 2);
             link.download = `QR_${button.dataset.id}.png`;
             link.click();
+        }
+
+        if (button.classList.contains('view-btn')) {
+            const batchId = button.dataset.batchId;
+            if (confirm('¿Estás seguro de que quieres eliminar este lote y todos sus sub-procesos?')) {
+                try {
+                    await api(`/api/batches/${batchId}`, { method: 'DELETE' });
+                    await loadBatches();
+                } catch (error) {
+                    alert('Error al eliminar el lote: ' + error.message);
+                }
+            }
         }
     }
 
