f16513b019bbfe4e38a29b766152a74296e2f21d
José García Burgos <jose.garcia.burgos@bbva.com>
2026-08-22 10:26:45 -0500

    .

--- src/controllers/landingsController.js
+++ src/controllers/landingsController.js
@@ -141,7 +141,7 @@ const getCompanyLandingData = async (req, res) => {
             perfil_data: safeJSONParse(p.perfil_data),
             notas_rueda: safeJSONParse(p.notas_json),
             recent_batches: batchesByProduct[p.id] || [],
-            atributos_dinamicos: safeJSONParse(p.atributos_dinamicos || '{}')
+            atributos_dinamicos: safeJSONParse(p.atributos_dinamicos)
         }));
 
         // 6. Enviar Respuesta
